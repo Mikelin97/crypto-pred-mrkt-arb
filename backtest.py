d6bfd9112f26ad9929140bbf04c0c9f2b53c8ebe
wjn7 <wjn7@duke.edu>
2025-11-15 21:27:43 -0500

    updating backtest for trading

--- backtest.py
+++ backtest.py
@@ -9,7 +9,7 @@ from OrderBook import OrderBook
 
 def backtest_market(crypto_data_filepath, crypto_data_filename, crypto_symbol, 
                     market_data_filepath, market_slug,
-                    unix_start_time, unix_end_time, 
+                    backtest_start_time, backtest_end_time, 
                     warm_up_duration, # how long to warm the models up before the start time
                     strategy:Strategy, # the strategy that is used 
                     time_precision: int = 13 # default milliseconds unix precision -> convert all timestamps to this
@@ -26,18 +26,22 @@ def backtest_market(crypto_data_filepath, crypto_data_filename, crypto_symbol,
         def adjust_precision(timestamp:int, precision:int) -> int:
             return int(np.ceil(timestamp * 10**(precision - len(str(timestamp)))))
         
-        unix_start_time = adjust_precision(unix_start_time, time_precision)
-        unix_end_time = adjust_precision(unix_end_time, time_precision)
+        backtest_start_time = adjust_precision(backtest_start_time, time_precision)
+        backtest_end_time = adjust_precision(backtest_end_time, time_precision)
+        warm_up_start_time = backtest_start_time - warm_up_duration
         # warm_up_duration = adjust_precision(warm_up_duration, time_precision) -> can't because it's a time difference
         
         # read in crypto data
-        all_coin_df = crypto_raw_to_pandas(f"{crypto_data_filepath}{crypto_data_filename}")
-        coin_df = all_coin_df[all_coin_df['symbol'] == crypto_symbol].sort_values(by="unix_timestamp").reset_index(drop=True)
+        all_crypto_df = crypto_raw_to_pandas(f"{crypto_data_filepath}{crypto_data_filename}")
+        crypto_df = all_crypto_df[all_crypto_df['symbol'] == crypto_symbol].sort_values(by="unix_timestamp").reset_index(drop=True)
 
         # get token IDs of the market
-        url = "https://gamma-api.polymarket.com/events"
-        response = requests.get(url, params={"slug": market_slug})
-        yes_token_id, no_token_id = json.loads(response.json()[0]['markets'][0]['clobTokenIds'])
+        try:
+            url = "https://gamma-api.polymarket.com/events"
+            response = requests.get(url, params={"slug": market_slug})
+            yes_token_id, no_token_id = json.loads(response.json()[0]['markets'][0]['clobTokenIds'])
+        except:
+            return None, None
 
         # get order book updates from our historical data
         messages = []
@@ -53,12 +57,14 @@ def backtest_market(crypto_data_filepath, crypto_data_filename, crypto_symbol,
         # get trades from both yes and no book perspectives
         trades_yes_perspective, trades_no_perspective = parse_all_trade_prices(messages, yes_token_id, no_token_id)
         
-        # create queues for time arrival
-        crypto_price_queue = deque(zip(coin_df['unix_timestamp'], coin_df['value']))
-        market_update_queue = deque(yes_book_updates)
-        # trade_queue = deque(trades_yes_perspective)  # TODO -> add trade data?
 
-        warm_up_start_time = unix_start_time - warm_up_duration
+        # create queues for time arrival
+        in_scope_crypto_df = crypto_df[(crypto_df['unix_timestamp'] >= warm_up_start_time) & (crypto_df['unix_timestamp'] <= backtest_end_time)]
+        crypto_price_queue = deque(zip(in_scope_crypto_df['unix_timestamp'], in_scope_crypto_df['value']))
+        market_update_queue = deque(yes_book_updates)
+        trade_queue = deque(trades_yes_perspective)
+
+        warm_up_start_time = backtest_start_time - warm_up_duration
 
         warm_up_data = []
         backtest_data = []
@@ -69,22 +75,26 @@ def backtest_market(crypto_data_filepath, crypto_data_filename, crypto_symbol,
             _, latest_crypto_price = crypto_price_queue.popleft()
         while market_update_queue and market_update_queue[0]['timestamp'] < warm_up_start_time:
             market_update_queue.popleft()
+        while trade_queue and trade_queue[0]['timestamp'] < warm_up_start_time:
+            trade_queue.popleft()
 
         # warm up strategy
-        while crypto_price_queue or market_update_queue:
+        while crypto_price_queue or market_update_queue or trade_queue:
             # pick whichever event occurs next
             next_times = []
             if crypto_price_queue:
                 next_crypto_time, _ = crypto_price_queue[0]
                 next_times.append(next_crypto_time)
-            
             if market_update_queue:
                 next_market_time = market_update_queue[0]['timestamp']
                 next_times.append(next_market_time)
+            if trade_queue:
+                next_trade_time = trade_queue[0]['timestamp']
+                next_times.append(next_trade_time)
             
             next_timestamp = min(next_times)
 
-            if next_timestamp >= unix_start_time:
+            if next_timestamp >= backtest_start_time:
                 # exit once we hit backtest time
                 break
                 
@@ -94,11 +104,16 @@ def backtest_market(crypto_data_filepath, crypto_data_filename, crypto_symbol,
             
             # update order book if needed
             market_updates = []
-            while market_update_queue[0]['timestamp'] == next_timestamp:
+            while market_update_queue and market_update_queue[0]['timestamp'] == next_timestamp:
                 market_update = market_update_queue.popleft()
                 market_updates.append(market_update)
+            
+            trades = []
+            while trade_queue and trade_queue[0]['timestamp'] == next_timestamp:
+                trade = trade_queue.popleft()
+                trades.append(trade)
 
-            strategy.on_warmup(next_timestamp, latest_crypto_price, market_updates)
+            strategy.on_warmup(next_timestamp, latest_crypto_price, market_updates, trades)
             warm_up_data.append({
                     "timestamp": next_timestamp,
                     **strategy.get_state()
@@ -120,7 +135,7 @@ def backtest_market(crypto_data_filepath, crypto_data_filename, crypto_symbol,
             
             next_timestamp = min(next_times)
 
-            if next_timestamp > unix_end_time:
+            if next_timestamp > backtest_end_time:
                 # exit once we hit end of backtest
                 break
                 
@@ -130,11 +145,16 @@ def backtest_market(crypto_data_filepath, crypto_data_filename, crypto_symbol,
             
             # update order book if needed
             market_updates = []
-            while market_update_queue[0]['timestamp'] == next_timestamp:
+            while market_update_queue and market_update_queue[0]['timestamp'] == next_timestamp:
                 market_update = market_update_queue.popleft()
                 market_updates.append(market_update)
+            
+            trades = []
+            while trade_queue and trade_queue[0]['timestamp'] == next_timestamp:
+                trade = trade_queue.popleft()
+                trades.append(trade)
 
-            strategy.on_update(next_timestamp, latest_crypto_price, market_updates)
+            strategy.on_update(next_timestamp, latest_crypto_price, market_updates, trades)
             backtest_data.append({
                     "timestamp": next_timestamp,
                     **strategy.get_state()
